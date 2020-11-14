358aa2cb1337a31ca2eb7b87e87f367f312cf942
Leon Sczepansky <leon.sczepansky@gmail.com>
2020-11-15 01:38:43 +0800

    signup of player

--- backend/src/dealer/table/table.py
+++ backend/src/dealer/table/table.py
@@ -1,7 +1,7 @@
 """ This are the classes needed for the Poker Game """
 from typing import List
 from enum import Enum, auto
-from dealer.player import player
+from dealer.player.player import Player
 
 class Phase(Enum):
     REGISTRATION = auto() # Game has not started. Player able to take a seat.
@@ -16,6 +16,13 @@ class Table():
     def __init__(self, cash_per_player : float = 500,
             small_blind :float = 10, big_blind :float = 20,
             max_players :int = 8):
+
+        if max_players > 22:
+            raise ValueError("Maximum for a Poker Game is is 22 players.")
+
+        if max_players < 2:
+            raise ValueError("Minimum for a Poker Game is 2 players.")
+
         self.cash_per_player :float = float(cash_per_player)
         self.small_blind :float = float(small_blind)
         self.big_blind :float = float(big_blind)
@@ -23,8 +30,8 @@ class Table():
 
         self.phase :Phase = Phase.REGISTRATION
 
-        self.players :List[player.Player]
-        self.community_cards :List[str]
+        self.players :List[Player] = []
+        self.community_cards :List[str] = []
 
     def json(self):
         return {
@@ -32,11 +39,23 @@ class Table():
             "cash_per_player": self.cash_per_player,
             "small_blind" : self.small_blind,
             "big_blind" : self.big_blind,
-            "phase": self.phase.name
+            "phase": self.phase
         }
 
     def start_game(self):
-        """Start the game, hand out money to each player"""
+        """ Start the game, hand out money to each player """
         if self.phase != Phase.REGISTRATION:
             raise RuntimeError("Game already started.")
         self.game_started = True
+
+    def signup(self, player :Player):
+        """ A player sits down at the table and gets the start budget"""
+        if self.phase != Phase.REGISTRATION:
+            raise RuntimeError("Game already started. No signups anymore.")
+        if len(self.players) >= self.max_players:
+            raise RuntimeError("Max players reached. No signups anymore.")
+        else:
+            self.players.append(player)
+            player.money_seat = self.cash_per_player
+
+
